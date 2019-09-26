466d04d909c2bb05220f6c2ceec57d990e519e51
Kasper Mikiewicz <IderedPL@gmail.com>
2019-09-26 14:41:28 +0200

    chore: format

--- webpack.config.js
+++ webpack.config.js
@@ -10,28 +10,28 @@ module.exports = (env, argv) => ({
 
   entry: {
     ui: './src/ui.tsx', // The entry point for your UI code
-    code: './src/code.ts', // The entry point for your plugin code
+    code: './src/code.ts' // The entry point for your plugin code
   },
 
   module: {
     rules: [
       // Converts TypeScript code to JavaScript
-      { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+      {test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/},
 
       // Enables including CSS by doing "import './file.css'" in your TypeScript code
-      { test: /\.css$/, loader: [{ loader: 'style-loader' }, { loader: 'css-loader' }] },
+      {test: /\.css$/, loader: [{loader: 'style-loader'}, {loader: 'css-loader'}]},
 
       // Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
-      { test: /\.(png|jpg|gif|webp|svg|zip)$/, loader: [{ loader: 'url-loader' }] },
-    ],
+      {test: /\.(png|jpg|gif|webp|svg|zip)$/, loader: [{loader: 'url-loader'}]}
+    ]
   },
 
   // Webpack tries these extensions for you if you omit the extension like "import './file'"
-  resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js'] },
+  resolve: {extensions: ['.tsx', '.ts', '.jsx', '.js']},
 
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'), // Compile into a folder called "dist"
+    path: path.resolve(__dirname, 'dist') // Compile into a folder called "dist"
   },
 
   // Tells Webpack to generate "ui.html" and to inline "ui.ts" into it
@@ -40,8 +40,8 @@ module.exports = (env, argv) => ({
       template: './src/ui.html',
       filename: 'ui.html',
       inlineSource: '.(js)$',
-      chunks: ['ui'],
+      chunks: ['ui']
     }),
-    new HtmlWebpackInlineSourcePlugin(),
-  ],
+    new HtmlWebpackInlineSourcePlugin()
+  ]
 })
